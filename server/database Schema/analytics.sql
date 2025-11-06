2d4df687256f2dc6866535eee57d5fda88f36e67
NaCryptoKe <NahomCryptoKetema@gmail.com>
2025-11-06 22:35:25 +0300

    added the sql code in appropriate place. /server/ database Schema

---
+++ server/database Schema/analytics.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA analytics;
+
+-- Table for storing high-volume game/user events
+CREATE TABLE analytics.game_telemetry (
+                                          id            BIGINT NOT NULL, -- PK component 1
+                                          game_id       UUID NOT NULL, -- No FK for performance, for decoupling
+                                          session_id    UUID,
+                                          user_id       UUID,
+                                          event_type    TEXT NOT NULL,
+                                          payload       JSONB,
+                                          timestamp     TIMESTAMPTZ NOT NULL DEFAULT now(), -- PK component 2 (the partition key)
+
+                                          PRIMARY KEY (id, timestamp)
+) PARTITION BY RANGE (timestamp);
