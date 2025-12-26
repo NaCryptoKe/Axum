1194f6ff36bff0e196446e8ed68ddb3d37b803d7
NaCryptoKe <NahomCryptoKetema@gmail.com>
2025-12-26 21:23:33 +0300

    REmoved the quotes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -53,8 +53,8 @@ const ARGON2_OPTS = {
 passport.use(
     new GoogleStrategy(
         {
-            clientID: "process.env.GOOGLE_CLIENT_ID",
-            clientSecret: "process.env.GOOGLE_CLIENT_SECRET",
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: "http://localhost:3000/api/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
