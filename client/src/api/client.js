aa9be495aac91af27a21894fcc0a592d778ecce3
NaCryptoKe <NahomCryptoKetema@gmail.com>
2026-01-26 20:43:08 +0300

    Update the base api

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = "https://axumm.up.railway.app/api";
 
 export async function apiRequest(path, options = {}) {
     const res = await fetch(`${API_BASE}${path}`, {
